141f5eb624adbb4d10a98783c60b736d901d248b
vamshireddy-81 <vsadureddy@gmail.com>
2022-10-18 13:07:34 +0530

    updated

--- application.java
+++ application.java
@@ -9,7 +9,14 @@ public static void main(String args[])
   n3=n1+n2;    
   System.out.print(" "+n3);    
   n1=n2;    
-  n2=n3;    
+  n2=n3;
+  int i,fact=1;  
+  int number=5;//It is the number to calculate factorial    
+  for(i=1;i<=number;i++){    
+      fact=fact*i;    
+  }    
+
+    
  }    
   
 }}  
